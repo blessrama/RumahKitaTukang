327fd44461a2eb058ec91e5ed73da413530d62de
Bless Ramadewa <bramasuteja@gmail.com>
2018-07-09 09:57:37 +0800

    added history and fragments for home also add several drawable items add several string too

---
+++ app/src/main/java/blessrama/pkm/rumahkitatukang/home_active.java
@@ -0,0 +1,13 @@
+package blessrama.pkm.rumahkitatukang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class home_active extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_home_active);
+    }
+}
